b8f317cbc463ccbce80ea899ddf85d3edde479d5
Prabhudev Poudel <prabhudevpoudel@Prabhudevs-iMac.local>
2026-07-02 13:20:13 -0500

    Add Soccer Daily X social link

--- constants/socialLinks.ts
+++ constants/socialLinks.ts
@@ -2,8 +2,10 @@ export const SOCCER_DAILY_SOCIAL_LINKS = {
   youtube: 'https://www.youtube.com/channel/UC-FNALunTqvcdrlFMo4nVfA',
   threads: 'https://www.threads.com/@soccerdailyapp2026',
   facebook: 'https://www.facebook.com/share/1JiWvgJp8V/',
+  x: 'https://x.com/soccerdaily2026?s=11&t=56yaHbnvbIVl2dTsfmF0LQ',
 };
 
 export const SOCCER_DAILY_YOUTUBE = SOCCER_DAILY_SOCIAL_LINKS.youtube;
 export const SOCCER_DAILY_THREADS = SOCCER_DAILY_SOCIAL_LINKS.threads;
 export const SOCCER_DAILY_FACEBOOK = SOCCER_DAILY_SOCIAL_LINKS.facebook;
+export const SOCCER_DAILY_X = SOCCER_DAILY_SOCIAL_LINKS.x;
